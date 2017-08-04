51962fe7279c69353384517d67e5c6e08affb33f
Roman Kopaev <roman@kopaev.ru>
2017-08-04 14:16:18 +0300

    no message

---
+++ Source/PZone.Xrm/Sdk/IOrganizationServiceExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+
+namespace PZone.Xrm.Sdk
+{
+    /// <summary>
+    /// Расширение функционала классов, реализующих интерфейс <see cref="IOrganizationService"/>.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public static class IOrganizationServiceExtensions
+    {
+        #region RetrieveMultiple
+
+
+        /// <summary>
+        /// Получение списка записей по запросу FetchXML.
+        /// </summary>
+        /// <param name="service">Экземпляр сервиса CRM.</param>
+        /// <param name="fetchXml">Текст запроса.</param>
+        /// <returns>
+        /// Метод возвращает список найденных записей.
+        /// </returns>
+        public static EntityCollection RetrieveMultiple(this IOrganizationService service, string fetchXml)
+        {
+            return service.RetrieveMultiple(new FetchExpression(fetchXml));
+        }
+
+
+        #endregion
+
+
+        #region Execute
+
+
+        /// <summary>
+        /// Выполнение запроса и получение ответа указанного типа.
+        /// </summary>
+        /// <param name="service">Экземпляр сервиса CRM.</param>
+        /// <param name="request">Запрос.</param>
+        /// <typeparam name="TResponse">Тип ответа запроса.</typeparam>
+        /// <returns>
+        /// Метод выполняет запрос и возвращает ответ указанного типа.
+        /// </returns>
+        public static TResponse Execute<TResponse>(this IOrganizationService service, OrganizationRequest request) where TResponse : OrganizationResponse
+        {
+            return (TResponse)service.Execute(request);
+        }
+
+
+        #endregion
+    }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,5 @@
 # PZone SDK XRM
+Common assembly for MS Dynamics development.
 <ul>
-<li><a href="v8/">v8: Dynamics CRM 2016 / Dynamics 365</a></li>
+<li><a href="v8/"><b>v8</b>: Dynamics CRM 2016 / Dynamics 365</a></li>
 </ul>
