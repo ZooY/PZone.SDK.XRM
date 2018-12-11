1a6bb726ae2b41ff53df0558a93734afab820f5d
Роман Копаев <roman@kopaev.ru>
2018-12-11 12:01:32 +0300

    no message

---
+++ Source/PZone.Xrm/CrmDateTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace PZone.Xrm
+{
+    /// <summary>
+    /// CRM specified DateTime.
+    /// </summary>
+    public class CrmDateTime
+    {
+        /// <summary>
+        /// Minimum correct CRM DateTime.
+        /// </summary>
+        public static DateTime MinValue = new DateTime(1753, 1, 1);
+    }
+}
